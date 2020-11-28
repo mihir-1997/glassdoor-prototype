d5f8a3c7614855725f061f8f6c0c3120e7b1f2ab
jainishsjsu <jainishjitendrakumar.parikh@sjsu.edu>
2020-11-27 19:20:43 -0800

    Functionality to get salary stats by job title

--- kafka-backend/services/contributions/getSalariesByEmployer.js
+++ kafka-backend/services/contributions/getSalariesByEmployer.js
@@ -7,33 +7,46 @@ function handle_request ( msg, callback ) {
     }
     if ( req.body.firstTime ) {
         contributionsSchema.find( { $and: [ { "employerName": req.body.params.employerName }, { "type": "salary" } ] } ).then( doc => {
-            var min = 1000000
-            var max = 0
+            var salary_by_job = {
+
+            }
             var count = 0
             doc.forEach( sal => {
                 count += 1
                 let salary = JSON.parse( JSON.stringify( sal ) )
-                if ( salary.baseSalary > max ) {
-                    max = salary.baseSalary
+                if ( salary.jobTitle in salary_by_job ) {
+                    if ( salary.baseSalary > salary_by_job[ salary.jobTitle ].max ) {
+                        salary_by_job[ salary.jobTitle ].max = salary.baseSalary
+                    }
+                    if ( salary.baseSalary < salary_by_job[ salary.jobTitle ].min ) {
+                        salary_by_job[ salary.jobTitle ].min = salary.baseSalary
+                    }
+
+                } else {
+                    salary_by_job[ salary.jobTitle ] = {
+                        min: salary.baseSalary,
+                        max: salary.baseSalary
+                    }
+
                 }
-                if ( salary.baseSalary < min ) {
-                    min = salary.baseSalary
-                }
+
             } )
 
             let stats = {
                 totalCount: count,
-                minimum_salary: min,
-                maximum_salary: max
+                salary_by_jobTitle: salary_by_job,
+
             }
             contributionsSchema.find( { $and: [ { "employerName": req.body.params.employerName }, { "type": "salary" } ] } ).sort( { salaryDate: 'desc' } ).skip( 0 ).limit( req.body.pageSize ).then( doc => {
 
                 callback( null, JSON.stringify( { salaryStats: stats, salary: doc } ) )
             } ).catch( error => {
+                console.log( error )
                 callback( error, null )
             } )
 
         } ).catch( error => {
+            console.log( error )
             callback( error, null )
         } )
 
