64fed983a5456e4afc0bafd09933ef6a479c706f
Mihir <mihirpatel121997@gmail.com>
2020-12-02 14:37:25 -0800

    refactor(code): search interview and salary

--- frontend/src/components/Employer/Salaries/EmployerSalaries.js
+++ frontend/src/components/Employer/Salaries/EmployerSalaries.js
@@ -16,8 +16,8 @@ class EmployerSalaries extends Component {
 
         super( props )
         this.state = {
-            salaries:{},
-            logoImageUrl:""
+            salaries: {},
+            logoImageUrl: ""
         }
     }
     componentDidMount () {
@@ -33,7 +33,7 @@ class EmployerSalaries extends Component {
                 .then( ( res ) => {
                     //console.log(res)
                     if ( res.status === 200 ) {
-                        this.setState( {                           
+                        this.setState( {
                             logoImageUrl: res.data.logoImageUrl,
                         } )
                     }
@@ -49,18 +49,25 @@ class EmployerSalaries extends Component {
                 } )
         }
 
-        let name = localStorage.getItem( "name" )
+        let name = null
+        if ( this.props.location ) {
+            if ( this.props.location.state ) {
+                name = this.props.location.state.searchTerm
+            } else {
+                name = localStorage.getItem( "name" )
+            }
+        }
         axios.defaults.withCredentials = true
         axios.defaults.headers.common[ 'authorization' ] = localStorage.getItem( 'token' )
-        axios.post( BACKEND_URL + ":" + BACKEND_PORT + "/contributions/getSalariesByEmployer/" + name,{ "firstTime":true,"pageNumber":1 ,"pageSize":100} )
+        axios.post( BACKEND_URL + ":" + BACKEND_PORT + "/contributions/getSalariesByEmployer/" + name, { "firstTime": true, "pageNumber": 1, "pageSize": 100 } )
             .then( ( res ) => {
-                 
+
                 if ( res.status === 200 ) {
-                    
-                    this.setState( {                           
+
+                    this.setState( {
                         salaries: res.data.salaryStats.salary_by_jobTitle
                     } )
-        
+
                 }
             } )
             .catch( ( err ) => {
@@ -74,105 +81,105 @@ class EmployerSalaries extends Component {
             } )
     }
 
-render() {
-   
-    let IndividualSalary = (key,obj) =>{
-        return(
-            <div key={key} style={{border:"1px solid black", padding:"10px", marginBottom:"3px"}}>
-            <div>
-                <div style={{display:"inline-block"}} className="col-4"> <span style={{fontSize:"25px"}}>{key}</span> </div>
-                <div style={{display:"inline-block"}} className="col-4">
-                    <svg style={{left:"200px"}} class="SVGInline-svg" height="32" viewBox="0 0 6 32" width="6" xmlns="http://www.w3.org/2000/svg"><path d="M1 .86l4 15.492m0 0L1 32" fill="none" stroke="#c4c7cc" stroke-linecap="square"></path></svg>
-                    <span style={{marginLeft:"80px"}}> 
-                        <div style={{display:"inline-block", fontStyle:"sans-serif", fontSize:"18px"}}>
-                        <span style={{fontStyle:"sans-serif",fontWeight:"bold", color:"green"}}>Min</span>
-                        <span style={{fontWeight:"bold"}}>{obj.min}</span>
-                        </div> 
-                </span>   
-                </div>                            
-                <div style={{display:"inline-block"}} className="col-4">
-                    <svg class="SVGInline-svg" height="32" viewBox="0 0 6 32" width="6" xmlns="http://www.w3.org/2000/svg"><path d="M1 .86l4 15.492m0 0L1 32" fill="none" stroke="#c4c7cc" stroke-linecap="square"></path></svg>                   
-                    <span style={{marginLeft:"80px"}}> 
-                        <div style={{display:"inline-block", fontStyle:"sans-serif", fontSize:"18px"}}>
-                        <span style={{fontStyle:"sans-serif",fontWeight:"bold", color:"green"}}>Max</span>
-                        <span style={{fontWeight:"bold"}}>{obj.max}</span>
-                        </div> 
-                </span>
+    render () {
+
+        let IndividualSalary = ( key, obj ) => {
+            return (
+                <div key={ key } style={ { border: "1px solid black", padding: "10px", marginBottom: "3px" } }>
+                    <div>
+                        <div style={ { display: "inline-block" } } className="col-4"> <span style={ { fontSize: "25px" } }>{ key }</span> </div>
+                        <div style={ { display: "inline-block" } } className="col-4">
+                            <svg style={ { left: "200px" } } class="SVGInline-svg" height="32" viewBox="0 0 6 32" width="6" xmlns="http://www.w3.org/2000/svg"><path d="M1 .86l4 15.492m0 0L1 32" fill="none" stroke="#c4c7cc" stroke-linecap="square"></path></svg>
+                            <span style={ { marginLeft: "80px" } }>
+                                <div style={ { display: "inline-block", fontStyle: "sans-serif", fontSize: "18px" } }>
+                                    <span style={ { fontStyle: "sans-serif", fontWeight: "bold", color: "green" } }>Min</span>
+                                    <span style={ { fontWeight: "bold" } }>{ obj.min }</span>
+                                </div>
+                            </span>
+                        </div>
+                        <div style={ { display: "inline-block" } } className="col-4">
+                            <svg class="SVGInline-svg" height="32" viewBox="0 0 6 32" width="6" xmlns="http://www.w3.org/2000/svg"><path d="M1 .86l4 15.492m0 0L1 32" fill="none" stroke="#c4c7cc" stroke-linecap="square"></path></svg>
+                            <span style={ { marginLeft: "80px" } }>
+                                <div style={ { display: "inline-block", fontStyle: "sans-serif", fontSize: "18px" } }>
+                                    <span style={ { fontStyle: "sans-serif", fontWeight: "bold", color: "green" } }>Max</span>
+                                    <span style={ { fontWeight: "bold" } }>{ obj.max }</span>
+                                </div>
+                            </span>
+                        </div>
+
+
+                    </div>
+
                 </div>
-
-               
-            </div>                   
-           
-       </div>
-        )
-    }
-
-    let displaySalaries =() =>{
-        console.log("Inside")
-        const phrase = [];
-        for (var key in this.state.salaries) {
-            // skip loop if the property is from prototype
-            if (!this.state.salaries.hasOwnProperty(key)) continue;
-        
-            var obj = this.state.salaries[key];
-
-            console.log(key)
-            console.log(obj.min, obj.max)
-            
-            phrase.push(IndividualSalary(key,obj))
-      
+            )
         }
-        return phrase
-    }
-    
-    let redirectVar = null
+
+        let displaySalaries = () => {
+            console.log( "Inside" )
+            const phrase = [];
+            for ( var key in this.state.salaries ) {
+                // skip loop if the property is from prototype
+                if ( !this.state.salaries.hasOwnProperty( key ) ) continue;
+
+                var obj = this.state.salaries[ key ];
+
+                console.log( key )
+                console.log( obj.min, obj.max )
+
+                phrase.push( IndividualSalary( key, obj ) )
+
+            }
+            return phrase
+        }
+
+        let redirectVar = null
         if ( !localStorage.getItem( "active" ) ) {
             redirectVar = <Redirect to="/login" />
             return redirectVar
         }
-    return(
+        return (
 
-        <div className="employer-profile-wrapper">
-        {redirectVar}
-        <div className="root-header">
-            <div className="image-wrapper">
-                <img className="cover" src={salaryCover} alt="Cover"  />
-            </div>
-            <div className="details-wrapper">
-                    <div className="employer-company-logo">
-                        <img className="logo" src={BACKEND_URL + ":" + BACKEND_PORT + "/public/images/profilepics/" + this.state.logoImageUrl} alt="logo"/>
+            <div className="employer-profile-wrapper">
+                {redirectVar }
+                <div className="root-header">
+                    <div className="image-wrapper">
+                        <img className="cover" src={ salaryCover } alt="Cover" />
                     </div>
-                <div className="details">
-                    <h3 style={{marginTop:"10px"}}> {localStorage.getItem("name")} </h3>
-                    <br/>
-                    <br/>
-                </div>
-                <div className="row multiple-links">
-                    <div className="col-1.2 single-link"><a href="/employer/profile">Overview</a> </div> 
-                    <div className="col-1.2 single-link"><a href="/employer/reviews">Reviews</a> </div>
-                    <div className="col-1.2 single-link"><a href="/employer/jobs">Jobs</a> </div>
-                    <div className="col-1.2 single-link"><a href="/employer/salaries">Salaries</a> </div>
-                    <div className="col-1.2 single-link"><a href="/employer/interviews">Interviews</a> </div>
-                    <div className="col-1.2 single-link"><a href="/employer/photos">Photos</a> </div>
-                    <div className="col-1.2 single-link"><a href="/employer/reports">Reports</a> </div>
-                    
-                </div>
-            </div>   
-            <div className="salary-info-wrapper">
-            <p style={{marginLeft:"1px",fontSize:"30px", lineHeight:"27px", fontWeight:"bold"}}> {localStorage.getItem("name")} Salaries</p>
-            <hr/>
-            
-           
-             <div style={{marginBottom:"3px", overflowY:"auto", height:"500px"}}>
-                {displaySalaries()}
-                  
-            </div>
-        </div> 
-        </div>
-        
-    </div>
+                    <div className="details-wrapper">
+                        <div className="employer-company-logo">
+                            <img className="logo" src={ BACKEND_URL + ":" + BACKEND_PORT + "/public/images/profilepics/" + this.state.logoImageUrl } alt="logo" />
+                        </div>
+                        <div className="details">
+                            <h3 style={ { marginTop: "10px" } }> { localStorage.getItem( "name" ) } </h3>
+                            <br />
+                            <br />
+                        </div>
+                        <div className="row multiple-links">
+                            <div className="col-1.2 single-link"><a href="/employer/profile">Overview</a> </div>
+                            <div className="col-1.2 single-link"><a href="/employer/reviews">Reviews</a> </div>
+                            <div className="col-1.2 single-link"><a href="/employer/jobs">Jobs</a> </div>
+                            <div className="col-1.2 single-link"><a href="/employer/salaries">Salaries</a> </div>
+                            <div className="col-1.2 single-link"><a href="/employer/interviews">Interviews</a> </div>
+                            <div className="col-1.2 single-link"><a href="/employer/photos">Photos</a> </div>
+                            <div className="col-1.2 single-link"><a href="/employer/reports">Reports</a> </div>
 
-    )
+                        </div>
+                    </div>
+                    <div className="salary-info-wrapper">
+                        <p style={ { marginLeft: "1px", fontSize: "30px", lineHeight: "27px", fontWeight: "bold" } }> { localStorage.getItem( "name" ) } Salaries</p>
+                        <hr />
+
+
+                        <div style={ { marginBottom: "3px", overflowY: "auto", height: "500px" } }>
+                            { displaySalaries() }
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        )
     }
 }
 
