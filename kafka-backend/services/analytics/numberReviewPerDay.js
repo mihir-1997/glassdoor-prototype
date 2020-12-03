649cbf14ad98167bbc52f44f10530dbfc89a1342
jainishsjsu <jainishjitendrakumar.parikh@sjsu.edu>
2020-12-03 14:47:27 -0800

    Updated reviews per day

--- kafka-backend/services/analytics/numberReviewPerDay.js
+++ kafka-backend/services/analytics/numberReviewPerDay.js
@@ -8,14 +8,18 @@ function handle_request ( msg, callback ) {
 
     contributionsSchema.find( { "type": "review" } ).then( doc => {
         var reviews_per_day = {
-            0: 0,
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
-            5: 0,
-            6: 0
+
         }
+        var today = Date.now()
+        for ( let i = 0; i < 7; i++ ) {
+            let dateform = new Date( today - ( 86400000 * i ) )
+            let day = dateform.getDate();
+            let month = dateform.getMonth() + 1;
+            // let year = dateform.getFullYear();
+            let date = month + "/" + day
+            reviews_per_day[ date ] = 0
+        }
+
         var reviews_by_companies = {
 
         }
@@ -25,11 +29,20 @@ function handle_request ( msg, callback ) {
         var ceo_rating = {
 
         }
+        console.log( "reviews", reviews_per_day )
         //var count = doc.length
         var today = Date.now()
         doc.forEach( rev => {
             let review = JSON.parse( JSON.stringify( rev ) )
-            reviews_per_day[ Math.floor( ( today - review.reviewDate ) / 86400000 ) ] += 1
+            let dateform = new Date( review.reviewDate )
+            let day = dateform.getDate();
+            let month = dateform.getMonth() + 1;
+            //let year = dateform.getFullYear();
+            let date = month + "/" + day
+            if ( date in reviews_per_day ) {
+                reviews_per_day[ date ] += 1
+            }
+            // reviews_per_day[ Math.floor( ( today - review.reviewDate ) / 86400000 ) ] += 1
             if ( review.employerName in reviews_by_companies ) {
                 reviews_by_companies[ review.employerName ].total += 1
                 reviews_by_companies[ review.employerName ].total_rating += review.rating
@@ -59,8 +72,6 @@ function handle_request ( msg, callback ) {
                 }
 
             }
-            console.log( "company", reviews_by_companies )
-
 
             if ( review.studentID in reviews_by_students ) {
                 reviews_by_students[ review.studentID ] += 1
@@ -68,8 +79,15 @@ function handle_request ( msg, callback ) {
                 reviews_by_students[ review.studentID ] = 1
             }
         } )
+        let perday = []
+        for ( let day in reviews_per_day ) {
+            perday.push( {
+                "name": day,
+                "Reviews/Day": reviews_per_day[ day ]
+            } )
+        }
 
-        callback( null, JSON.stringify( { analytics: { reviews_per_day: reviews_per_day, reviews_by_companies: reviews_by_companies, reviews_by_students: reviews_by_students, ceo_rating: ceo_rating } } ) )
+        callback( null, JSON.stringify( { analytics: { reviews_per_day: perday, reviews_by_companies: reviews_by_companies, reviews_by_students: reviews_by_students, ceo_rating: ceo_rating } } ) )
     } ).catch( error => {
         callback( error, null )
     } )
